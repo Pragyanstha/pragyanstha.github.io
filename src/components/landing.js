40ecb8a959eff5b648a48a20280c706c00afe0f1
Pragyanstha <pragyanstha@gmail.com>
2021-11-14 21:14:03 +0900

    Landing page : first content created

---
+++ src/components/landing.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const landing = () => {
+    return (
+        <div className="wrapper">
+            <nav>
+                <ul>
+                    <li>LOGO</li>
+                    <li>Tech Blog</li>
+                    <li>Projects</li>
+                    <li>About</li>
+                    <li>SNS</li>
+                </ul>
+            </nav>
+            <div className="banner">
+                <div className="banner_introduction">
+                    <h1>Pragyan <br />&nbsp;&nbsp;&nbsp;Shrestha</h1>
+                    <p>
+                    I am a graduate student at University of Tsukuba. My research interests are in medical imaging and computer vision.
+                    </p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+
+export default landing
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Landing from '../components/landing'
+
+// markup
+const IndexPage = () => {
+  return (
+      <Landing />
+  )
+}
+
+export default IndexPage

f61fc88c117011ca36b23ea60ac746426741342c
Pragyanstha <pragyanstha@gmail.com>
2021-11-16 00:16:31 +0900

    Changed Layout

--- public/render-page.js
+++ public/render-page.js
@@ -4736,24 +4736,24 @@ if (false) {} else {
 /***/ }),
 
 /***/ "react-dom/server":
-/*!*************************************************************************************!*\
-  !*** external "/Users/pragyan/Projects/portfolio/node_modules/react-dom/server.js" ***!
-  \*************************************************************************************/
+/*!************************************************************************************!*\
+  !*** external "/home/pragyan/Projects/portfolio/node_modules/react-dom/server.js" ***!
+  \************************************************************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("/Users/pragyan/Projects/portfolio/node_modules/react-dom/server.js");
+module.exports = require("/home/pragyan/Projects/portfolio/node_modules/react-dom/server.js");
 
 /***/ }),
 
 /***/ "react":
-/*!********************************************************************************!*\
-  !*** external "/Users/pragyan/Projects/portfolio/node_modules/react/index.js" ***!
-  \********************************************************************************/
+/*!*******************************************************************************!*\
+  !*** external "/home/pragyan/Projects/portfolio/node_modules/react/index.js" ***!
+  \*******************************************************************************/
 /***/ ((module) => {
 
 "use strict";
-module.exports = require("/Users/pragyan/Projects/portfolio/node_modules/react/index.js");
+module.exports = require("/home/pragyan/Projects/portfolio/node_modules/react/index.js");
 
 /***/ })
 
--- src/components/landing.js
+++ src/components/landing.js
@@ -1,33 +1,43 @@
 import React from 'react';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faLinkedinIn, faInstagram } from "@fortawesome/free-brands-svg-icons";
-
-
+import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
+import ProfilePic from "../images/profile_pic.jpg"
+import IconPic from "../images/icon.svg"
 const landing = () => {
     
     return (
         <div className="wrapper">
             <nav>
                 <ul>
-                    <li>LOGO</li>
+                    <li><img className="icon" src={IconPic}></img></li>
                     <li>Tech Blog</li>
                     <li>Publications</li>
                     <li>About</li>
-                    <li className="sns"><FontAwesomeIcon icon={faLinkedinIn} /></li>
-                    <li className="sns"><FontAwesomeIcon icon={faInstagram} /></li>
+                    <li className="sns"><FontAwesomeIcon className="linkedin" icon={faLinkedinIn} /></li>
+                    <li className="sns"><FontAwesomeIcon className="github" icon={faGithub} /></li>
                 </ul>
     
             </nav>
             <div className="banner">
                 <div className="banner_introduction">
+                    <h2>
+                    <bold>Hi</bold>, I am
+                    </h2>
                     <h1>Pragyan <br />Shrestha</h1>
                     <p>
-                    Hi, I am a graduate student at University of Tsukuba. 
+                    I am a graduate student at University of Tsukuba. 
                     My research interests are in medical imaging and computer vision.
                     Thanks for visiting!
                     </p>
                 </div>
+
+                <div className="banner_profile">
+                    <img className="banner_profile-pic" src={ProfilePic}></img>
+                </div>
             </div>
+
+
+            
         </div>
     )
 }
